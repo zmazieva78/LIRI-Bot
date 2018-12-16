eea36b109a282a5e89133f44b2e938315afb2cd7
Zarina Mazieva <zmazieva78@gmail.com>
2018-12-15 19:24:55 -0800

    final push

--- README.md
+++ README.md
@@ -5,15 +5,15 @@ LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and R
 
 LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
 
-* spotify-this-song  ``` node liri.js spotify-this-song ``` 
+ **spotify-this-song**  ``` node liri.js spotify-this-song ``` 
 - This will show the following information about the song in your terminal/bash window:
     - Artist(s)
     - The song's name
     - A preview link of the song from Spotify
     - The album that the song is from.
 
-* movie-this ``` node liri.js movie-this ```           
-- This will output the following information to your   terminal/bash window:
+ **movie-this** ``` node liri.js movie-this ```           
+- This will output the following information to your terminal/bash window:
 
     - Title of the movie.
     - Year the movie came out.
@@ -24,6 +24,7 @@ LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for mov
     - Plot of the movie.
     - Actors in the movie.
 
-* do-what-it-says node ``` liri.js do-what-it-says ```
+ **do-what-it-says** ``` node liri.js do-what-it-says ```
 
-- Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands. -It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt. Feel free to change the text in that document to test out the feature for other commands.
+- Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands. 
+    - It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt. Feel free to change the text in that document to test out the feature for other commands.
--- liri.js
+++ liri.js
@@ -3,74 +3,108 @@ var Spotify = require('node-spotify-api');
 var keys = require('./keys.js');
 var request = require("request");
 var moment = require('moment');
+var fs = require('fs');
+var log4js = require('log4js');
+
+log4js.configure({
+    appenders: {
+        liri_file: { type: 'file', filename: 'log.txt' },
+        liri_console: { type: 'console' }
+    },
+    categories: {
+        default: {
+            appenders: ['liri_file'],
+            level: 'debug'
+        }
+    }
+});
+
+const logger = log4js.getLogger('liri');
+
+logger.info("liri");
+
 var args = process.argv;
 
-if (args.length != 4) {
-    console.log('Usage:');
-    console.log('\tnode liri.js <command> <command-arg>');
-    console.log('commands:');
-    console.log('\tconcert-this');
-    console.log('\tspotify-this-song');
-    console.log('\tmovie-this');
-    console.log('\tdo-what-it-says');
+if (args.length < 3 || args.length > 4) {
+    const usage = 'Usage:' + 
+    '\n\tnode liri.js <command> <command-arg>' + 
+    '\ncommands:' +
+    '\n\tconcert-this' +
+    '\n\tspotify-this-song' +
+    '\n\tmovie-this' +
+    '\n\tdo-what-it-says';
+
+    console.log(usage);
 
     return;
 }
 
-var command = args[2];
-var command_arg = args[3];
+const command = args[2];
+const command_arg = args[3];
 
-if (command === 'spotify-this-song') {
-    console.log('Query spotify here: ' + command_arg);
+executeCommand(command, command_arg);
 
+function spotifyThis(command_arg) {
     var spotify = new Spotify(keys.spotify);
 
     spotify.search({ type: 'track', query: command_arg }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log("=========================================================" +
-            "\nArtist: " + data.tracks.items[0].artists[0].name +
-            "\nSong's Name: " + data.tracks.items[0].name +
-            "\nAlbum the Song is From: " + data.tracks.items[0].album.name +
-            "\nPreview Link of the Song: " + data.tracks.items[0].preview_url +
-            "\n=========================================================");
+
+        const output = "\n=========================================================" +
+        "\n*** spotify-this '" + command_arg + "' ***" +
+        "\n\nArtist: " + data.tracks.items[0].artists[0].name +
+        "\nSong's Name: " + data.tracks.items[0].name +
+        "\nAlbum the Song is From: " + data.tracks.items[0].album.name +
+        "\nPreview Link of the Song: " + data.tracks.items[0].preview_url;
+
+        console.log(output);
+        logger.info(output);
     });
-} else if (command === "movie-this") {
+}
+
+function movieThis(command_arg) {
     request("http://www.omdbapi.com/?t=" + command_arg + "&y=&plot=short&apikey=b1f8f1d7", function (error, response, body) {
 
         // If the request is successful (i.e. if the response status code is 200)
         if (!error && response.statusCode === 200) {
 
             // Parse the body of the site and recover the title, year, IMBD rating, Rotten Tomatoes rating, Country, Language, Plot and Actors from the movie
-            console.log("=========================================================" +
-                "\nTitle of the Movie: " + JSON.parse(body).Title +
-                "\nYear: " + JSON.parse(body).Year +
-                "\nIMBD Rating: " + JSON.parse(body).imdbRating +
-                "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1] +
-                "\nCountry: " + JSON.parse(body).Country +
-                "\nLanguage: " + JSON.parse(body).Language +
-                "\nPlot: " + JSON.parse(body).Plot +
-                "\nActors: " + JSON.parse(body).Actors +
-                "\n=========================================================\n");
+            const result = JSON.parse(body);
+        
+            const output = "\n=========================================================" +
+            "\n*** movie-this '" + command_arg + "' ***" +
+            "\n\nTitle of the Movie: " + result.Title +
+            "\nYear: " + result.Year +
+            "\nIMBD Rating: " + result.imdbRating +
+            "\nRotten Tomatoes Rating: " + result.Ratings[1] +
+            "\nCountry: " + result.Country +
+            "\nLanguage: " + result.Language +
+            "\nPlot: " + result.Plot +
+            "\nActors: " + result.Actors;
 
+            console.log(output);
+            logger.info(output);
         };
     });
+}
 
-} else if (command === "concert-this") {
-    var artist = command_arg
-    console.log(artist);
-
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+function concertThis(artist) {
+    const queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     request(queryURL, function (error, response, body) {
-
         if (error) {
             return console.log('Error occurred: ' + err);
         }
 
+        const header = "\n=========================================================" +
+        "\n*** concert-this '" + artist + "' ***";
+
+        console.log(header);
+        logger.info(header);
+
         var result = JSON.parse(body);
-        // for loop for 10 results and display them
 
         var maxResults = Math.min(result.length, 10);
 
@@ -78,10 +112,43 @@ if (command === 'spotify-this-song') {
             var current = result[i];
             var venue = current.venue;
 
-            console.log("=========================================================")
-            console.log(venue.name);
-            console.log(venue.city + ', ' + venue.region + ', ' + venue.country);
-            console.log(moment(current.datetime).format("MM/DD/YYYY"));
-            }
+            const item = "\n\t=================================================" +
+            "\n\t" + venue.name +
+            "\n\t" + venue.city + ', ' + venue.region + ', ' + venue.country +
+            "\n\t" + moment(current.datetime).format("MM/DD/YYYY");
+
+            console.log(item);
+            logger.info(item);
+        }
     });
+}
+
+function doWhatItSays() {
+    fs.readFile('./random3.txt', 'utf8', function (err, contents) {
+        if (err) throw err;
+
+        var lines = contents.split('\n');
+
+        lines.forEach(function (line) {
+            var commandLine = line.split(',');
+            var command = commandLine[0];
+            var command_arg = commandLine[1].substring(1, commandLine[1].length - 1);
+
+            executeCommand(command, command_arg);
+        });
+    });
+}
+
+function executeCommand(command, command_arg) {
+    if (command === 'spotify-this-song') {
+        spotifyThis(command_arg);
+    } else if (command === "movie-this") {
+        movieThis(command_arg);
+    } else if (command === "concert-this") {
+        concertThis(command_arg);
+    } else if (command === "do-what-it-says") {
+        doWhatItSays();
+    } else {
+        console.error('Unknown command: ' + command);
+    }
 }
